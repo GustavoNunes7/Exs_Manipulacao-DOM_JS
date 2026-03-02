8c73d2d4093302c15d6ec63a08ed9b147546a226
Gustavo Nunes <Gununes.tech7@gmail.com>
2026-03-02 11:09:21 -0300

    Falta o Ex.9

--- script.js
+++ script.js
@@ -2,51 +2,68 @@
 let cor = document.getElementById("cor")
 let teclaTexto = document.getElementById("tecla")
 
+let cores ={
+    vermelho: "R",
+    verde : "G",
+    azul : "B",
+    amarelo: "Y",
+    roxo: "P",
+    escuro:"N",
+    claro: "D"
+}
 
 document.addEventListener("keydown" , function(e){
     let tecla = e.key.toLowerCase();
 
     if(tecla === "r"){
         document.body.style.backgroundColor = "red"
+         document.body.style.color= "white"
         cor.innerHTML = "Cor: Vermelho (Red)"
-        teclaTexto.innerHTML = "Tecla: R"
+        teclaTexto.innerHTML = `Tecla: ${cores.vermelho}` 
         console.log(e.key + " -> rgb(255, 0, 0)")
         
 
      } else if (tecla === "g"){
          document.body.style.backgroundColor = "green"
+          document.body.style.color= "white"
          cor.innerHTML = "Cor: Verde (Green)"
-         teclaTexto.innerHTML = "Tecla: g"
+         teclaTexto.innerHTML = `Tecla: ${cores.verde}`
          console.log(e.key + " -> rgb(0, 255, 0)")
 
     }else if (tecla === "b"){
          document.body.style.backgroundColor= "blue"
+          document.body.style.color= "white"
          cor.innerHTML = "Cor: Azul (Blue)"
-         teclaTexto.innerHTML = "Tecla: b"
+         teclaTexto.innerHTML = `Tecla: ${cores.azul}`
          console.log(e.key + " -> rgb(0, 0, 255)")
 
     }else if(tecla === "y") {
         document.body.style.backgroundColor= "yellow"
+         document.body.style.color= "black"
         cor.innerHTML = "Cor: Amarelo (Yellow)"
-        teclaTexto.innerHTML = "Tecla: y"
+        teclaTexto.innerHTML = `Tecla: ${cores.amarelo}`
         console.log(e.key + " -> rgb(255, 255, 0)")
 
     } else if (tecla === "p"){
         document.body.style.backgroundColor= "purple"
         cor.innerHTML = "Cor: Roxo (purple)"
-        teclaTexto.innerHTML = "Tecla: p"
+        teclaTexto.innerHTML = `Tecla: ${cores.roxo}`
         console.log(e.key + " -> rgb(128, 0, 128)")
-    }
-        
-})
-//Ex.7??????????????????
-let cores ={
-    vermelho: "r",
-    verde : "g",
-    azul : "b",
-    amarelo: "y",
-    roxo: "p"
-}
 
+    } else if (tecla === "n"){
+        document.body.style.backgroundColor= "black"
+        document.body.style.color= "white"
+        cor.innerHTML = "Modo Escuro"
+        teclaTexto.innerHTML = `Tecla: ${cores.escuro}`
+        console.log(e.key + " -> rgb(0 , 0 , 0)")
+
+    } else if (tecla === "d"){
+        document.body.style.backgroundColor= "white"
+         document.body.style.color= "black"
+        cor.innerHTML = "Modo Claro"
+        teclaTexto.innerHTML = `Tecla: ${cores.claro}`
+        console.log(e.key + " -> rgb(255 , 255 , 255)")
+    }
+})
 
 
